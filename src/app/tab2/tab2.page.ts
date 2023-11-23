7f6dc4309de031124704267c5594ed96221658c1
Aryan <aryanshah@192.168.1.10>
2023-11-23 12:28:03 +0530

    Ionic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { UserPhoto } from '../services/user-photo';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
-  constructor() {}
-
+export class Tab2Page implements OnInit {
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+  constructor(public photoService: PhotoService,public actionSheetController: ActionSheetController) {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
